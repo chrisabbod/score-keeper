c0dbfbdf09e316a8cfad3b0794668b05f1ec9ee1
Chris <chrisabbod@gmail.com>
2018-04-15 22:23:58 -0400

    <feat>: Add Display Function for Team A TextView
    Add function that displays added points to Team A's TextView and
    add touchdown function to add 6 points to Team A's score

--- app/src/main/java/com/example/android/scorekeeper/MainActivity.java
+++ app/src/main/java/com/example/android/scorekeeper/MainActivity.java
@@ -2,12 +2,27 @@ package com.example.android.scorekeeper;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    int teamAScore = 0;
+    int teamBScore = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+    }
+
+    public void displayForTeamA(int score){
+        TextView scoreView = (TextView)findViewById(R.id.team_a_score_text_view);
+        scoreView.setText(String.valueOf(score));
+    }
+    public void addTouchdownA(View v){
+        teamAScore = teamAScore + 6;
+        displayForTeamA(teamAScore);
     }
 }
